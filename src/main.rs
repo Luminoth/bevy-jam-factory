f1dd489433beebe9ced383a67b1d982f9bc38805
Shane Lillie <ignignokterr@gmail.com>
2024-07-19 21:48:43 -0700

    Some extra window setup

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ use bevy::prelude::*;
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins(DefaultPlugins);
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Bevy Jam - Factory".into(),
+            resolution: (1280.0, 720.0).into(),
+            ..default()
+        }),
+        ..default()
+    }));
 
     app.init_state::<states::AppState>()
         .add_sub_state::<states::IsPaused>()

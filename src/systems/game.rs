d61a014a8032fb2a455c2570e18662bb9d0fd460
Shane Lillie <ignignokterr@gmail.com>
2024-07-22 10:45:51 -0700

    Generate a basic tilemap

--- src/systems/game.rs
+++ src/systems/game.rs
@@ -1,7 +1,42 @@
 use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 
-pub fn setup_game(mut commands: Commands) {
-    println!("setup game");
-
+pub fn setup_game(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
+
+    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
+    let map_size = TilemapSize { x: 128, y: 128 };
+
+    let mut tile_storage = TileStorage::empty(map_size);
+    let map_type = TilemapType::Square;
+
+    let tilemap_entity = commands.spawn_empty().id();
+
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    ..Default::default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+    let grid_size = tile_size.into();
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        size: map_size,
+        storage: tile_storage,
+        map_type,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size,
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+        ..Default::default()
+    });
 }

b704e5e72fa461d74013b26872709172d1b5d0c9
Shane Lillie <ignignokterr@gmail.com>
2024-08-10 11:31:07 -0700

    Fixed scaling mode seems the move

--- src/systems/game.rs
+++ src/systems/game.rs
@@ -1,10 +1,16 @@
 use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
 
 use crate::assets::tiled::TiledMap;
 use crate::components::tiled::TiledMapBundle;
 
 pub fn setup_game(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((Camera2dBundle::default(), Name::new("Main Camera")));
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scaling_mode = ScalingMode::Fixed {
+        width: 800.0,
+        height: 600.0,
+    };
+    commands.spawn((camera, Name::new("Main Camera")));
 
     let map_handle: Handle<TiledMap> = asset_server.load("map.tmx");
 

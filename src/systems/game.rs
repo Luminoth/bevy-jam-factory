e76d23411697c94ce94224cc815a8a739679b24c
Shane Lillie <ignignokterr@gmail.com>
2024-08-10 11:36:58 -0700

    Mouse grab

--- src/systems/game.rs
+++ src/systems/game.rs
@@ -1,10 +1,15 @@
 use bevy::prelude::*;
 use bevy::render::camera::ScalingMode;
+use bevy::window::CursorGrabMode;
 
 use crate::assets::tiled::TiledMap;
 use crate::components::tiled::TiledMapBundle;
 
-pub fn setup_game(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn setup_game(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut windows: Query<&mut Window>,
+) {
     let mut camera = Camera2dBundle::default();
     camera.projection.scaling_mode = ScalingMode::Fixed {
         width: 800.0,
@@ -18,4 +23,8 @@ pub fn setup_game(mut commands: Commands, asset_server: Res<AssetServer>) {
         tiled_map: map_handle,
         ..Default::default()
     });
+
+    let mut window = windows.single_mut();
+    window.cursor.visible = false;
+    window.cursor.grab_mode = CursorGrabMode::Locked;
 }

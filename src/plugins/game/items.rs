23654017dc834404794d87c0f0094826fd6d643b
Shane Lillie <ignignokterr@gmail.com>
2024-08-29 08:55:14 -0700

    Kind of buggy still but first pass at coloring drag tiles / objects

--- src/plugins/game/items.rs
+++ src/plugins/game/items.rs
@@ -1,12 +1,19 @@
 use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 
 use crate::data::items::ItemData;
 use crate::get_world_position_from_cursor_position;
-use crate::plugins::{camera::MainCamera, TiledMapObjectLayer, TiledMapTileLayer};
+use crate::plugins::{camera::MainCamera, objects::Object, TiledMapObjectLayer, TiledMapTileLayer};
 use crate::tilemap::{get_tile_position, TileMapQuery};
 
 #[derive(Debug, Component, Deref)]
 pub struct Item(pub ItemData);
+
+#[derive(Debug, Resource)]
+pub struct ItemDragObject(pub Entity);
+
+#[derive(Debug, Resource)]
+pub struct ItemDragTile(pub Entity);
 
 #[derive(Debug, Event)]
 pub struct ItemDragEvent(pub Option<Vec2>);
@@ -26,11 +33,22 @@ impl ItemDropEvent {
     }
 }
 
+const NEGATIVE_COLOR: Color = Color::srgba(1.0, 0.0, 0.0, 0.5);
+
+// TODO: we might be able to simplify this by splitting it into
+// an object handler and a tile handler? would need to not consume the events for that
+// and would need to make sure we handle objects before tiles ...
+#[allow(clippy::too_many_arguments)]
 pub(super) fn item_drag_event_handler(
+    mut commands: Commands,
     mut events: EventReader<ItemDragEvent>,
     camera_query: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    mut drag_object: Option<ResMut<ItemDragObject>>,
     object_layer_query: Query<TileMapQuery, With<TiledMapObjectLayer>>,
+    mut object_query: Query<&mut TileColor, With<Object>>,
+    mut drag_tile: Option<ResMut<ItemDragTile>>,
     tilemap_layer_query: Query<TileMapQuery, With<TiledMapTileLayer>>,
+    mut tile_query: Query<&mut TileColor, Without<Object>>,
 ) {
     if events.is_empty() {
         return;
@@ -41,6 +59,8 @@ pub(super) fn item_drag_event_handler(
     // TODO: should we just deal with the first (or last?) event?
     // what does it even mean to have more than one of these ...
     for event in events.read() {
+        // TODO: pretty sure this is missing some edge cases
+
         let world_position =
             get_world_position_from_cursor_position(event.0, camera, camera_transform);
         if let Some(world_position) = world_position {
@@ -53,8 +73,28 @@ pub(super) fn item_drag_event_handler(
                 object_tilemap.r#type,
                 object_tilemap.transform,
             ) {
-                if let Some(_object_entity) = object_tilemap.storage.get(&object_position) {
-                    info!("object at {:?}", object_position);
+                if let Some(object_entity) = object_tilemap.storage.get(&object_position) {
+                    // reset and remove previous tile
+                    if let Some(drag_tile) = &drag_tile {
+                        let mut color = tile_query.get_mut(drag_tile.0).unwrap();
+                        color.0 = Color::default();
+                        commands.remove_resource::<ItemDragTile>();
+                    }
+
+                    // check previous object
+                    if let Some(drag_object) = &mut drag_object {
+                        if drag_object.0 == object_entity {
+                        } else {
+                            let mut color = object_query.get_mut(drag_object.0).unwrap();
+                            color.0 = Color::default();
+                            drag_object.0 = object_entity;
+                        }
+                        continue;
+                    }
+
+                    let mut color = object_query.get_mut(object_entity).unwrap();
+                    color.0 = NEGATIVE_COLOR;
+                    commands.insert_resource(ItemDragObject(object_entity));
                     continue;
                 }
             }
@@ -68,8 +108,28 @@ pub(super) fn item_drag_event_handler(
                 tilemap.r#type,
                 tilemap.transform,
             ) {
-                if let Some(_tile_entity) = tilemap.storage.get(&tile_position) {
-                    info!("tile at {:?}", tile_position);
+                if let Some(tile_entity) = tilemap.storage.get(&tile_position) {
+                    // reset and remove previous object
+                    if let Some(drag_object) = &drag_object {
+                        let mut color = object_query.get_mut(drag_object.0).unwrap();
+                        color.0 = Color::default();
+                        commands.remove_resource::<ItemDragObject>();
+                    }
+
+                    // check previous tile
+                    if let Some(drag_tile) = &mut drag_tile {
+                        if drag_tile.0 == tile_entity {
+                        } else {
+                            let mut color = tile_query.get_mut(drag_tile.0).unwrap();
+                            color.0 = Color::default();
+                            drag_tile.0 = tile_entity;
+                        }
+                        continue;
+                    }
+
+                    let mut color = tile_query.get_mut(tile_entity).unwrap();
+                    color.0 = NEGATIVE_COLOR;
+                    commands.insert_resource(ItemDragTile(tile_entity));
                     continue;
                 }
             }
